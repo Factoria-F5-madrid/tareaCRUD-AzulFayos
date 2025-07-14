46167d1cb7d7ea81f92a8886f33982ec5db376e3
Coder <azul.fayos@gmail.com>
2025-07-14 09:35:25 +0200

    chore: admin config

--- book_system/books/admin.py
+++ book_system/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class LibroAdmin(admin.ModelAdmin):
+  list_display = ('title', 'autor', 'publication_date', 'isbn')
+  search_fields = ('title', 'autor', 'isbn')
+  list_filter = ('publication_date',)
