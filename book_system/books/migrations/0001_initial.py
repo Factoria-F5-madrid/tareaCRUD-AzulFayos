e2c30071a5b6af4d0c1a0832a02421d3f9c26897
Coder <azul.fayos@gmail.com>
2025-07-14 12:44:48 +0200

    chore: indentation correction

--- book_system/books/migrations/0001_initial.py
+++ book_system/books/migrations/0001_initial.py
@@ -5,21 +5,21 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
+  initial = True
 
-    dependencies = [
-    ]
+  dependencies = [
+  ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Book',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('autor', models.CharField(max_length=100)),
-                ('description', models.TextField()),
-                ('publication_date', models.DateField()),
-                ('isbn', models.CharField(max_length=13, unique=True)),
-            ],
-        ),
-    ]
+  operations = [
+    migrations.CreateModel(
+      name='Book',
+      fields=[
+        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('title', models.CharField(max_length=200)),
+        ('autor', models.CharField(max_length=100)),
+        ('description', models.TextField()),
+        ('publication_date', models.DateField()),
+        ('isbn', models.CharField(max_length=13, unique=True)),
+      ],
+    ),
+  ]
--- book_system/books/urls.py
+++ book_system/books/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.books_list, name='books_list'),
-    path('<int:book_id>/', views.book_detail, name='book_detail'),
-    path('create/', views.create_book, name='create_book'),
-    path('<int:book_id>/edit/', views.book_edit, name='book_edit'),
-    path('<int:book_id>/delete/', views.book_delete, name='book_delete'),
+  path('', views.books_list, name='books_list'),
+  path('<int:book_id>/', views.book_detail, name='book_detail'),
+  path('create/', views.create_book, name='create_book'),
+  path('<int:book_id>/edit/', views.book_edit, name='book_edit'),
+  path('<int:book_id>/delete/', views.book_delete, name='book_delete'),
 ]
